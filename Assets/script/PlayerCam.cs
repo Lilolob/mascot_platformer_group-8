e7ba670e8c61bcea6c92e289e0f4a2686cbee8ea
Tobolob <tcgreg91@gmail.com>
2026-03-15 19:31:12 +0000

    added grappleing

--- Assets/script/PlayerCam.cs
+++ Assets/script/PlayerCam.cs
@@ -23,14 +23,6 @@ public class PlayerCam : MonoBehaviour
     private void Update()
     {
         // get mouse input
-        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
-
-        yRotation += mouseX;
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
         // rotate cam and orientation
         camHolder.rotation = Quaternion.Euler(xRotation, yRotation, 0);
         orientation.rotation = Quaternion.Euler(0, yRotation, 0);
